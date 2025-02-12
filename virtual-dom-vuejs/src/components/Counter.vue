103801a050daba8840697461c6b3fe5c1366768c
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2025-02-12 16:00:45 +0100

    fix: repair counter

--- virtual-dom-vuejs/src/components/Counter.vue
+++ virtual-dom-vuejs/src/components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="counter">
     <h2>Counter: {{ count }}</h2>
-    //TODO
+    <button @click="increment">Increment</button>
   </div>
 </template>
 
@@ -10,7 +10,7 @@ import { ref } from "vue";
 
 export default {
   setup() {
-    //TODO
+    const count = ref(0);
 
     const increment = () => {
       count.value++;
