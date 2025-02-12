9e94b968cf09f0c8aa3bef6577231db82a2a9477
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2025-02-12 15:53:47 +0100

    fix: repair counter

--- virtual-dom-react/src/components/Counter.jsx
+++ virtual-dom-react/src/components/Counter.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);
-
+    const increment = () => setCount(count + 1);
     return (
         <div className="counter">
             <h2>Counter: {count}</h2>
-            //TODO
+            <button onClick={increment}>Add count</button>
         </div>
     );
 }
